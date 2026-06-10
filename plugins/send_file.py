aae0d71fb2f6af5b928296459c5fd6c5edf8c703
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-06-10 23:08:41 +0530

    Update send_file.py

--- plugins/send_file.py
+++ plugins/send_file.py
@@ -1,6 +1,8 @@
 import asyncio
 from database.users_db import db
 from utils import temp, auto_delete_message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from info import PROTECT_CONTENT
 
 async def send_requested_file(client, message, user_id, search_id):
     try:
@@ -8,12 +10,18 @@ async def send_requested_file(client, message, user_id, search_id):
         if not file_data:
             return await message.reply("❌ File not found.")
 
+        # Next button add karo
+        next_btn = InlineKeyboardMarkup([
+            [InlineKeyboardButton("⏩ Next Video", callback_data="next_video")]
+        ])
+
         dlt = await message.reply_video(
             video=file_data['file_id'],
             caption=(
                 f"<i>𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘉𝘺: {temp.U_NAME}</i>\n\n"
                 f"<blockquote>ᴛʜɪꜱ ꜰɪʟᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴀꜰᴛᴇʀ 10 ᴍɪɴᴜᴛᴇꜱ. ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ ᴏʀ ꜱᴀᴠᴇ ɪɴ ꜱᴀᴠᴇᴅ ᴍᴇꜱꜱᴀɢᴇꜱ.</blockquote>"
-            )
+            ),
+            reply_markup=next_btn   # 👈 button yahan
         )
         asyncio.create_task(auto_delete_message(message, dlt))
 
