94bae9435ca29f2fa87336b4f6226bd5ffb31d14
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-06-17 16:26:27 +0530

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -10,6 +10,7 @@ from utils import temp, is_user_joined
 from plugins.verification import verify_user_on_start
 from plugins.send_file import send_requested_file
 from plugins.refer import refer_on_start
+from plugins.premium import approve_payment, reject_payment, payment_screenshot_handler
 
 # =================================================
 # 🚀 START COMMAND
