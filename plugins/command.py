9f07027acc3f7fa93b6dbfd00bca07cc41451dbd
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-06-20 13:44:43 +0530

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -74,7 +74,7 @@ async def start_command(client, message: Message):
          InlineKeyboardButton("ℹ️ About", callback_data="about")]
     ])
 
-    await message.reply_photo(
+   send_msg = await message.reply_photo(
         photo=START_PIC,
         caption=script.START_TXT.format(mention, temp.U_NAME, temp.U_NAME),
         reply_markup=buttons,
