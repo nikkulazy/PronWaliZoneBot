3284af2f894feefc3a94aa3af381b6791bd30c59
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-06-28 17:29:12 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -78,10 +78,7 @@ QR_CODE_IMAGE = environ.get("QR_CODE_IMAGE", "https://i.ibb.co/kWtBcgx/photo-202
 # =========================================================
 # 🖼️ IMAGES
 # =========================================================
-START_PICS = [
-    "http://ibb.co/zCGSdbR", 
-    "https://i.ibb.co/dwycT3zJ/photo-2026-06-28-09-18-32-7656373422435336208.jpg",
-]
+START_PIC = environ.get("START_PIC", "http://ibb.co/zCGSdbR")
 AUTH_PICS = environ.get("AUTH_PICS", "http://ibb.co/zCGSdbR")
 VERIFY_IMG = environ.get("VERIFY_IMG", "http://ibb.co/zCGSdbR")
 NO_IMG = environ.get("NO_IMG", "http://ibb.co/zCGSdbR")
