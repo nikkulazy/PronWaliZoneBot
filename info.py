ce29c86e86790dd78634cdbabd250b1a198c24c6
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-06-28 23:22:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,10 +64,10 @@ FREE_VIDEO_DURATION = 60
 # =========================================================
 # 🔗 SHORTLINK & VERIFICATION
 # =========================================================
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "")
-SHORTLINK_API = environ.get("SHORTLINK_API", "")
-POST_SHORTLINK_URL = environ.get("POST_SHORTLINK_URL", "")
-POST_SHORTLINK_API = environ.get("POST_SHORTLINK_API", "")
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "omegalinks.in")
+SHORTLINK_API = environ.get("SHORTLINK_API", "a7ac9b3012c67d7491414cf272d82593c75f6cbb")
+POST_SHORTLINK_URL = environ.get("POST_SHORTLINK_URL", "omegalinks.in")
+POST_SHORTLINK_API = environ.get("POST_SHORTLINK_API", "a7ac9b3012c67d7491414cf272d82593c75f6cbb")
 VERIFY_EXPIRE = int(environ.get("VERIFY_EXPIRE", "3600"))
 TUTORIAL_LINK = environ.get("TUTORIAL_LINK", "")
 
