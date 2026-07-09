32d8f3ed79fcc9a3ee8905d635274227d62fb57c
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-07-09 14:10:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,6 +58,7 @@ PROTECT_CONTENT = str_to_bool(environ.get("PROTECT_CONTENT"), True)
 DAILY_LIMIT = int(environ.get("DAILY_LIMIT", "5"))
 VERIFICATION_DAILY_LIMIT = int(environ.get("VERIFICATION_DAILY_LIMIT", "20"))
 PREMIUM_DAILY_LIMIT = int(environ.get("PREMIUM_DAILY_LIMIT", "50"))
+FREE_VIDEO_DURATION = int(environ.get("FREE_VIDEO_DURATION", "180"))  # 240 seconds = 4 minutes
 
 # =========================================================
 # 🔗 SHORTLINK & VERIFICATION
