0a507f4e16b32de87b5741448d344ed52fd35a74
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-07-11 00:14:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ OWNER_USERNAME = environ.get("OWNER_USERNAME", "WOLVERIN_P")
 # =========================================================
 # 💾 DATABASE CONFIGURATION
 # =========================================================
-DB_URL = environ.get("DATABASE_URI", "")
+DB_URL = environ.get("DATABASE_URI", "mongodb+srv://mastitime:mastitime@cluster0.tohbael.mongodb.net/?appName=Cluster0")
 DB_NAME = environ.get("DATABASE_NAME", "avbotz")
 
 # =========================================================
